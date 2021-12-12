cdfa94de50548cd9c9181ffc3dcb8bcef17cbb9c
Jaskaran Singh (js1040) <js1040@trigonometry2.stat.duke.edu>
2021-12-12 00:37:22 -0500

    v

--- R/summaryPlots.R
+++ R/summaryPlots.R
@@ -4,6 +4,7 @@ library(ggplot2)
 plays = read.csv(file = paste(getwd(),"/data/plays.csv",sep=''), header = TRUE)
 players = read.csv(file = paste(getwd(),"/data/players.csv",sep=''), header = TRUE)
 tracking = readRDS(paste0(getwd(),"/data/sample_play.rds"))
+pff = read.csv(paste0(getwd(),"/data/PFFScoutingData.csv"))
 
 plays %>%
   select(specialTeamsPlayType,kickLength) %>%
@@ -40,7 +41,39 @@ plays %>%
   ggplot(aes(y=possessionTeam,x=kickReturnYardage,fill=specialTeamsPlayType))+
   geom_bar(stat="identity")
 
+plays %>%
+  select(kickReturnYardage,ret,specialTeamsPlayType) %>%
+  ggplot(aes(y=possessionTeam,x=kickReturnYardage,fill=specialTeamsPlayType))+
+  geom_bar(stat="identity")
+
+pff %>% ggplot(aes(x=hangTime,y=..count..))+
+  geom_histogram(fill="blue")
+
+table(pff$kickDirectionIntended,pff$kickDirectionActual)
+
+plays <- plays %>%
+  left_join(pff %>% select(gameId,playId,snapDetail,
+                           kickDirectionIntended,kickDirectionActual),
+            by=c("gameId","playId"),snapDetail=snapDetail)
+
+c(snapDetail,kickAccuracy=(kickDirectionIntended==kickDirectionActual))
+
+plays %>% group_by(kickerId) %>%
+  summarize(correctPunt=sum(snapDetail == "OK", na.rm = TRUE), tot=n()) %>%
+  mutate(snapRatio=correctPunt/tot) %>%
+  arrange(desc(snapRatio)) %>%
+  left_join(players %>% select(kickerId=nflId,displayName),by=c("kickerId")) %>%
+  slice(1:10) %>%
+  ggplot(aes(y=displayName,x=snapRatio))+
+  geom_bar(stat="identity")
 
 
-
+plays %>% group_by(kickerId) %>%
+  summarize(kickAccuracy=sum(kickDirectionIntended==kickDirectionActual,na.rm = TRUE), tot=n()) %>%
+  mutate(kickAccuracy=kickAccuracy/tot) %>%
+  arrange(desc(kickAccuracy)) %>%
+  left_join(players %>% select(kickerId=nflId,displayName),by=c("kickerId")) %>%
+  slice(1:10) %>%
+  ggplot(aes(y=displayName,x=kickAccuracy))+
+  geom_bar(stat="identity")
 
